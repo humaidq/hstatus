c72b145167caa723cd2f54ab3753a3dd99018dd4
Humaid AlQassimi <git@humaidq.ae>
2020-04-12 03:58:45 +0400

    Add disp to a structure with set_status function and destructor.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,35 @@
 extern crate x11;
 use std::ffi::CString;
 
-fn main() {
-    let disp: *mut x11::xlib::Display;
-    // Open display
-    disp = unsafe {
-        x11::xlib::XOpenDisplay(std::ptr::null())
-    };
+pub struct DesktopStatus {
+    disp: *mut x11::xlib::Display,
+}
 
-    unsafe {
-        let s = CString::new("data data data data").expect("CString::new failed");
-        x11::xlib::XStoreName(disp, x11::xlib::XDefaultRootWindow(disp), s.as_ptr());
+impl DesktopStatus {
+    pub fn new() -> Self {
+        DesktopStatus {disp: unsafe {
+            x11::xlib::XOpenDisplay(std::ptr::null())
+        }}
     }
-    
-    unsafe {
-        x11::xlib::XCloseDisplay(disp);
+    pub fn set_status(self, stat: &str) {
+        unsafe {
+            let s = CString::new(stat).expect("CString::new failed at set_status");
+            x11::xlib::XStoreName(self.disp, x11::xlib::XDefaultRootWindow(self.disp), s.as_ptr());
+        }
     }
+}
+
+impl Drop for DesktopStatus {
+    fn drop(&mut self) {
+        unsafe {
+            x11::xlib::XCloseDisplay(self.disp);
+        }
+    }
+}
+
+fn main() {
+    // Open display
+    let status: DesktopStatus = DesktopStatus::new();
+    status.set_status("hi");
 }
 
